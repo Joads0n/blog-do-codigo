76fc571de950c6ff70f8e64825cef7866caaec43
Joadson Silva <joadson83@hotmail.com>
2022-06-21 18:27:01 -0300

    Refactoring email prepare production

--- src/usuarios/email.js
+++ src/usuarios/email.js
@@ -1,16 +1,38 @@
+const { process_params } = require('express/lib/router');
 const nodemailer = require('nodemailer');
 
+const emailProdConfig = {
+    host: process.env.EMAIL_HOST,
+    auth: {
+        user: process.env.EMAIL_USER,
+        password: process.env.EMAIL_PASSWORD
+    },
+    secure: true
+};
+
+const emailTestConfig = (accountTest) => ({
+    host: 'smtp.ethereal.email',
+    auth: accountTest,
+});
+
+async function createEmailConfig(){
+    if(process.env.NODE_ENV === 'production'){
+        return emailProdConfig;
+    } else {
+        const accountTest = await nodemailer.createTestAccount();
+        return emailTestConfig(accountTest);
+    }
+}
+
 class Email {
-    
     async sendEmail() {
-        const acountTest = await nodemailer.createTestAccount();
-        const transport = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            auth: acountTest,
-        });
+        const emailConfig = await createEmailConfig();
+        const transport = nodemailer.createTransport(emailConfig);
         const info = await transport.sendMail(this);
     
-        console.log('URL: ' + nodemailer.getTestMessageUrl(info));
+        if(process.env.NODE_ENV !== 'production'){
+            console.log('URL: ' + nodemailer.getTestMessageUrl(info));
+        }
     }
 }
 
